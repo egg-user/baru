a4ea8c82a21f8234da78cd3e5980747ef3948d37
egg-user <egakuswardana@gmail.com>
2023-12-08 21:58:31 +0700

    menambah get user dan enkripsi password

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const { request } = require('http');
+const bcrypt = require('bcrypt');
+const app = express();
+// const { request } = require('http');
 
+//koneksi ke database
 const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
@@ -11,13 +14,22 @@ const connection = mysql.createConnection({
 	database : 'nodelogin'
 });
 
-const app = express();
+connection.connect((err) => {
+	if (err) {
+	  console.error('Koneksi database gagal:', err);
+	  return;
+	}
+	console.log('Terhubung ke database');
+  });
+
+
 
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
-}));
+})); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
@@ -30,16 +42,18 @@ app.get('/', function(request, response) {
 app.post('/login', (request, response) => {
     const username = request.body.username;
     const password = request.body.password;
-
-    if (username && password) {
-        authenticateUser(username, password, (error, results) => {
+	const email = request.body.email;
+    if ((username || email) && password) {
+        authenticateUser(username, email, password, (error, results) => {
             if (error) {
                 response.status(500).json({ error: 'Internal Server Error' });
             } else {
                 if (results.length > 0) {
                     request.session.loggedin = true;
                     request.session.username = username;
+                    request.session.email = email;
                     response.redirect('/home');
+                    // response.redirect('/username/:email');
                 } else {
                     response.status(401).json({ error: 'Incorrect Username and/or Password!' });
                 }
@@ -52,15 +66,43 @@ app.post('/login', (request, response) => {
     }
 });
 
-//mendapatkan username yang sedang login
+//mendapatkan pesan setelah login
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
+		response.send('Welcome back!!');
 	} else {
 		response.send('Please login to view this page!');
 	}
 	response.end();
 });
+
+//api setelah login
+app.get('/api/protected', (request, result) => {
+	if (request.session.loggedin) {
+	  result.json({ message: 'Halaman terproteksi' });
+	} else {
+	  result.status(401).json({ message: 'Akses ditolak. Silakan login terlebih dahulu.' });
+	}
+  });
+
+
+// Endpoint untuk mendapatkan data berdasarkan username
+app.get('/username/:email', (req, res) => {
+	const requestedUsername = req.params.email;
+	// Query SQL dengan WHERE clause untuk mendapatkan data berdasarkan username
+	const query = 'SELECT * FROM accounts WHERE email = ?';
+	connection.query(query, [requestedUsername], (err, results) => {
+	  if (err) {
+		console.error('Kesalahan query:', err);
+		res.status(500).send('Terjadi kesalahan server');
+	  } else {
+		res.json({
+			username: results[0].username,
+		});
+	  }
+	});
+  });
+  
 
 app.post('/logout', (req, res) => {
 	// Hapus informasi otentikasi dari session
@@ -112,22 +154,20 @@ app.delete('/delete', (request, response) => {
 	}
 })
 
-//api setelah login
-app.get('/api/protected', (req, res) => {
-	if (req.session.user) {
-	  res.json({ message: 'Halaman terproteksi' });
-	} else {
-	  res.status(401).json({ message: 'Akses ditolak. Silakan login terlebih dahulu.' });
-	}
-  });
 
-
-function authenticateUser(username, password, callback) {
-    connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], callback);
+  function authenticateUser(username, email, password, callback) {
+    const connectionQuery = 'SELECT * FROM accounts WHERE (username = ? OR email = ?) AND password = ?';
+    connection.query(connectionQuery, [username, email, password], callback);
 }
 
 function registerUser(username, password, email, callback) {
-    connection.query('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)', [username, password, email], callback);
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        connection.query('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)', [username, hash, email], callback);
+    });
 }
 
 function deleteUser(username, password, email, callback) {
